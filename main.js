a7579367d4803d0292d9d8100321781b4298ddb3
hamza-sadeq <hamzasadeq41@gmail.com>
2023-01-31 14:37:23 +0200

    Landscaping

---
+++ main.js
@@ -0,0 +1,23 @@
+function downNav(){
+  document.getElementById("submenu-hamburgar").style.display ="block";
+}
+
+function closeNav(){
+  document.getElementById("submenu-hamburgar").style.display ="none";
+}
+
+function downNav1(){
+  document.getElementById("submenu-hamburgar2").style.display = "block"
+}
+
+function closeNav1(){
+  document.getElementById("submenu-hamburgar2").style.display = "none"
+}
+
+function openSidebar(){
+document.getElementById("nav-menu").style.width = "100%";
+}
+
+function CloseMenuBar(){
+document.getElementById("nav-menu").style.width = "0";
+}
